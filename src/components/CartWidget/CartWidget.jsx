99b1e567e92465d3a15e3ef5ea6d320867f69440
JulietaMaffioni23 <julietamaffioni@hotmail.com>
2023-08-07 20:26:32 -0300

    segundo commit

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -3,7 +3,7 @@ import cart from './assets/cart4.svg'
 const CartWidget = () => {
     return (
         <div>
-           <button class="btn" type="submit"> 
+           <button className="btn" type="submit"> 
            <img src={cart} alt="cart-widget"/>
            </button>
            <h5>0</h5>
---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,29 @@
+import CartWidget from "../CartWidget/CartWidget"
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <h3>Julietta Beauty</h3>
+          <div className="container-fluid"> 
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-2 mr-3">
+                <il>
+                <button className="btn" type="submit">Perfumes</button>
+                </il>
+                <li className="nav-item">
+                <button className="btn" type="submit">Herramientas</button>
+                </li>
+                <li className="nav-item">
+                <button className="btn" type="submit">Cosmetica</button>
+                </li>
+            </ul>
+          </div>
+         </div>
+            <CartWidget />
+        </nav>
+    )
+}
+
+export default NavBar
+
+
